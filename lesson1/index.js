56438331fab836a71513adf2e3bb74598d6f981d
vovanolo <vovchik.student221@gmail.com>
2020-09-19 14:23:32 +0300

    lesson1

---
+++ lesson1/index.js
@@ -0,0 +1,101 @@
+// console.log("Hello world!");
+
+// Змінні в js
+
+// let var const
+// викор let const
+
+// const yearOfBirth = 2004;
+// let age = 16;
+// let message = 'Hello world!';
+// console.log(yearOfBirth);
+// console.log(message);
+// yearOfBirth = 2006;
+// console.log(age);
+// age = 15;
+// console.log(age);
+
+// let age;
+// console.log(age);
+// // undefined
+
+// console.log(message);
+// undeclareted
+
+// типи змінних
+
+// числові змінні
+
+// const age = 20;
+// const number = 5.8;
+
+// стрічкові змінні
+
+// const name = 'Ivan';
+
+// console.log(name);
+
+
+// булеві змінні, логічні змінні
+
+// const auth = true;
+// const notAuth = false;
+
+// true = 1;
+// false = 0;
+
+// console.log(auth);
+
+// NULL
+
+// let someinfo = null;
+
+// console.log(someinfo);
+
+
+// undefined
+
+// let name;
+// console.log(name);
+
+
+// Функція type of
+
+// let a;
+// console.log(typeof a);
+
+// let b = null;
+// console.log(typeof b);
+
+// let c = 5;
+// console.log(typeof c);
+
+// let d = 'Js is good';
+// console.log(typeof d);
+
+
+// вивід даних
+
+// console.log()  alert() prompt()
+
+// alert('Hello world');
+
+// let a = prompt('enter your age');
+// console.log(a);
+
+// Математичні операції
+
+// const x = 10;
+// const y = 4;
+
+// console.log(x+y);
+//  console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+
+
+
+
+

d419a9a16d4863c590de63bd2d54e8192023a906
vovanolo <vovchik.student221@gmail.com>
2020-09-26 13:33:43 +0300

    lesson2

---
+++ lesson2/index.js
@@ -0,0 +1,114 @@
+// let const
+
+// const number = 10;
+// const message = "message";
+
+// const x = 10;
+// const y = 5;
+
+// let value = 10;
+
+// value--;
+// value = value + 1;
+
+// value = value + 10;
+// console.log(value);
+
+// Порівняння
+
+// const x = 5;
+// const y = 10;
+// const z = 5;
+
+// > < <= >= === !==
+
+// console.log(x > y);
+// console.log(x < y);
+// console.log(x >= z);
+// console.log(x === z);
+// console.log(x !== y);
+
+// обєкт Math
+
+// console.log(Math.floor(4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.round(4.4));
+// console.log(Math.max(10, 20, 30, 40, 60, 4, 5, 8, 11));
+// console.log(Math.min(10, 20, 30, 40, 60, 4, 5, 8, 11));
+
+// console.log(Math.round(Math.random() * 100));
+
+// стрічки або речення
+
+// const message = "hello " + "world " + "for " + "everyone";
+
+// console.log(message);
+
+// const message = "Welcome";
+
+// const info = message.toUpperCase();
+// const info = message.toLowerCase();
+
+// 0,1,2,3,4,5
+
+// console.log(message.indexOf("c"));
+
+// const name = "ivan";
+// const age = 16;
+// const mood = "happy";
+
+// // let message = "hello i am " + name + " im " + age + " years old and im " + mood;
+// let message = `hello i am ${name} im ${age} years old and im ${mood}`;
+// console.log(message);
+
+// Оператори умови
+
+// if else/ else if
+
+// let cost = 0;
+
+// const player = "blavlab";
+
+// if (player === "pro") {
+//   cost = 100;
+// } else if (player === "middle") {
+//   cost = 70;
+// } else if (player === "beginer") {
+//   cost = 60;
+// } else {
+//   console.log("error");
+// }
+// console.log(cost);
+
+// Тернарний оператор
+
+// let age = 17;
+
+// let result = умова ? вираз якщо умова виконується : вираз якщо не вионується
+
+// let result = age < 18 ? "ви не повнолітній" : "ви повнолітній";
+
+// if (age < 18) {
+//   result = "ви не повнолітній";
+// } else {
+//   result = "ви повнолітній";
+// }
+
+// console.log(result);
+
+// конструкція switch
+
+// let country = "таджикистан";
+
+// switch (country) {
+//   case "китай":
+//     console.log("привіт по китайський");
+//     break;
+//   case "індія":
+//     console.log("привіт по індійськи");
+//     break;
+
+//   default:
+//     console.log("я такої країни не знаю");
+//     break;
+// }

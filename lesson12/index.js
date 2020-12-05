ff3a110a1dae25e56f7c9e95d0a983e4015803a9
vovanolo <vovchik.student221@gmail.com>
2020-12-05 14:01:30 +0200

    lesson12

---
+++ lesson12/index.js
@@ -0,0 +1,74 @@
+// const text = document.getElementById("text");
+// text.hidden = true; //робимо текст невидимим
+
+// const message = document.querySelector("#message");
+// console.log(message.value);
+
+// const link = document.querySelector(".active");
+// console.log(link.href);
+
+// const image = document.querySelector(".image");
+
+// console.log(image.src);
+
+// image.src =
+//   "https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop";
+
+// const title = document.querySelector(".title");
+// title.textContent = "Welcome to my best friend site";
+
+// const text = document.querySelector(".text");
+// text.textContent = "bla bla bla";
+// console.log(text.textContent);
+
+// const paragraph = document.querySelector("#paragraph");
+
+// console.log(paragraph.classList);
+
+// console.log(paragraph.classList.contains("red"));
+
+// paragraph.classList.remove("big");
+
+// console.log(paragraph.classList);
+
+// paragraph.classList.add("new-class");
+// console.log(paragraph.classList);
+
+// paragraph.classList.add("a", "b", "c");
+// console.log(paragraph.classList);
+
+// paragraph.classList.toggle("hidden");
+// paragraph.classList.toggle("hidden");
+
+// console.log(paragraph.classList);
+
+// paragraph.classList.forEach(function (cls) {
+//   console.log(cls);
+// });
+
+// const image = document.querySelector(".image");
+
+// console.log(image.attributes);
+
+// console.dir(image);
+
+// console.log(image.hasAttribute("src"));
+
+// console.log(image.getAttribute("alt"));
+// console.log(image.alt);
+
+// image.setAttribute("alt", "this is a new cat");
+// image.alt = "this is a new cat";
+
+// const item = document.createElement("a");
+// // console.log(item);
+// item.href = "#";
+// item.classList.add("btn");
+// item.textContent = "item4";
+
+// const nav = document.querySelector(".nav");
+// nav.appendChild(item);
+
+// Дз створити елементр img(зі всіма атрибутами src) за допомогою javascript і вставити його в список li
+// Змінити клас тегу img на інший та за допомогою js спробувати змінити всі атрибути
+// так само спробувати створити якісь свої 4 теги в js і вставити їх  в html
